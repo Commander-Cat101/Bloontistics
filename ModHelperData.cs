70cd07dc4ea52a2397b75f61f409a791efcd233a
Commander-Cat101 <102434202+Commander-Cat101@users.noreply.github.com>
2022-08-28 16:56:12 +1200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,12 @@
+namespace Bloontistics;
+
+public static class ModHelperData
+{
+    public const string Version = "1.0.0";
+    public const string Name = "Bloontistics";
+
+    public const string Description = "Adds ingame stats.";
+
+    public const string RepoOwner = "Commander-Cat101"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "Bloontistics"; // TODO add your repo name here, also in the download url in README.md
+}
