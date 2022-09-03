bba716ff0ed01db3b87485a08d552886982284cb
Commander-Cat101 <102434202+Commander-Cat101@users.noreply.github.com>
2022-09-03 14:27:25 +1200

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@ namespace Bloontistics;
 
 public static class ModHelperData
 {
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.0";
     public const string Name = "Bloontistics";
 
     public const string Description = "Adds ingame stats.";
